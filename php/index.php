52c3adf917248f36bce828ad1e48a299ab79cd39
Voyager <startcev-german@mail.ru>
2020-03-29 15:05:45 +0300

    php folder commit

---
+++ php/index.php
@@ -0,0 +1,24 @@
+<?php
+	declare(strict_types=1);
+	
+	function getSum(int $x, int $y) {
+		return $x + $y;
+	}
+	
+	$x = 5;
+	$y = 7;
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+	<head>
+		<title>php lessons</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<div>
+			<?= "$x + $y = getSum($x, $y)" ?>
+		</div>
+	</body>
+</html>
+	
