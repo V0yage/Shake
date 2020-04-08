4979e09e4c290fe8830d43e65dc02036d6befe10
User A0423305 <a0423305@ysetur.from.sh>
2020-04-08 03:09:03 +0300

    mail sendler

--- php/mail_sender.php
+++ php/mail_sender.php
@@ -1,18 +1,35 @@
 <?php
 	function sendMail() {
-		var_dump($_POST);
+		$to = $_POST['to'];
+		$from = $_POST['from'];
+		$subj = $_POST['subj'];
+		$msg = $_POST['msg'];
+		$headers = "From: $from" . "\r\n" .
+					"Reply-To: $from" . "\r\n";
+		if (mail($to, $subj, $msg, $headers)) {
+			echo '<p style="color:seagreen;">Success!</p>';
+		} else {
+			echo '<p style="color:red;">End with error.</p>';
+		}
 	}
-?>	
-<?php if (empty($_POST)): ?>
-	<form action="/php/mail_sender.php">
-		<p>
-			Кому:<br>
-			<input type="text" name="to" value="german-startcev@ya.ru">
-		</p>
-		<p>
-			Сообщение:<br>
-			<textarea name="msg">Сообщение</textarea>
-		</p>
-	</form>
-<?php else: sendMail(); endif; ?>
-
+?>
+<html>
+	<head></head>
+	<body>
+	<?php if (empty($_POST)): ?>
+		<form action="/php/mail_sender.php" method="POST">
+			<p>
+				Кому:<br>
+				<input type="text" name="to" value="german-startcev@ya.ru">
+			</p>
+			<p>
+				Сообщение:<br>
+				<textarea name="msg">Сообщение</textarea>
+			</p>
+			<input type="hidden" name="from" value="admin@webshake.loc">
+			<input type="hidden" name="subj" value="Test message">
+			<input type="submit" value="Send">
+		</form>
+	<?php else: sendMail(); endif; ?>
+	</body>
+</html>
